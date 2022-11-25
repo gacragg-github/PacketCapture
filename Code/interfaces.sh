65f9d41517c22f627bd241bed7ded2206d69d593
gacragg <gacrag@gmail.com>
2022-11-25 11:56:08 -0500

    Add capability to estimate packet rate

--- Code/interfaces.sh
+++ Code/interfaces.sh
@@ -2,7 +2,7 @@
 
 #Protoype:
 #
-# ./interfaces.sh
+# ./interfaces.sh [options]
 #
 #Rollup interface wireless interface data.  Target output:
 #Ndx      Iface   Phy     Driver          Mode            Up?     Channel         Width   Packets
@@ -12,17 +12,62 @@
 #3        wlan6   phy1    rt2800usb       monitor         Y       6 (2437MHz)     20MHz   4629
 #
 #Maintainer: G. Cragg, gacragg@gmail.com
-#
-#	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-
-
-
 #Version	Date		Notes
 #0			Oct 2017	Release - note bug that if HT not enabled, output will be partially garbled
 #						So set interface to do HT, such as  
 #							iw dev wlp1s0 set channel 6 HT20
 #							iw dev wlp1s0 set channel 1 HT40+
 #0.1		Nov 2017	Added adapter ID from airmon-ng output
+#0.2		Nov 2022	Add delta packets capability to detect hung adapter
+
+
+
+#Defaults
+deltaperioddefault=3
+debug=false
+
+#Help
+usage () {
+	echo "Describe wireless interfaces"
+	echo "Usage: $0 [-d xxx]"
+	echo "Options: (all optional)"
+	echo -e "  -h                   Help"
+	echo -e	"  -d <xxx>             Display delta packet count over xxx period of sec"
+}
+
+
+#Check for elevated privileges
+if [[ $(id -u) -ne 0 ]]
+then 
+	echo "Note: More information is available with elevated privileges"
+	echo
+	#usage
+	#exit 1
+fi
+
+
+########################################################################
+#Option handling
+
+while getopts "h?d:" opt; do
+	case "$opt" in
+		h|\?)
+			usage
+			exit 0
+		;;
+		d)
+			if [ $OPTARG -eq $OPTARG 2>/dev/null -o $OPTARG -eq 0 2>/dev/null ]
+			then
+				deltaperiod=$OPTARG
+				if [ "${debug}" = true ]; then echo "[CONFIG] deltaperiod set to argument ${deltaperiod}"; fi
+			else
+				deltaperiod=${deltaperioddefault}
+				if [ "${debug}" = true ]; then echo "[CONFIG] deltaperiod set to default ${deltaperiod}"; fi 
+			fi
+			calcdeltapkts=true
+        ;;
+	esac
+done
 
 
 #Print an array of information
@@ -95,7 +140,7 @@ do
 	Status="Y"
    fi
    #strOUTPUT[$i]="${strOUTPUT[$i]}\t $Status"
-   strOUTPUT[$i]=$(printf "%s %5s" "${strOUTPUT[$i]}" "$Status")	
+   strOUTPUT[$i]=$(printf "%s %3s" "${strOUTPUT[$i]}" "$Status")	
 done
 
 
@@ -105,7 +150,7 @@ do
    channel=$(/sbin/iw ${IFACES[$i]} info | awk '/channel/ {print $0}' | awk -F" " '{print$2" "$3$4}' | sed 's/,$//')
    bandwidth=$(/sbin/iw ${IFACES[$i]} info | awk '/channel/ {print $0}' | awk -F" " '{print $6$7}' | sed 's/,$//')
    #strOUTPUT[$i]="${strOUTPUT[$i]}\t $channel\t $bandwidth"
-   strOUTPUT[$i]=$(printf "%s %13s %5s" "${strOUTPUT[$i]}" "$channel" "$bandwidth")	
+   strOUTPUT[$i]=$(printf "%s %13s %6s" "${strOUTPUT[$i]}" "$channel" "$bandwidth")	
 done
 
 
@@ -118,12 +163,39 @@ do
 done
 
 #Get packets seen on the interface
+rxpkts_0=()		#Initial packet count array
 for i in "${!IFACES[@]}"
 do
-   packets=$(/sbin/ifconfig ${IFACES[$i]} | awk '/RX packets/ {print $3}')
-   #strOUTPUT[$i]="${strOUTPUT[$i]}\t $packets"	
-   strOUTPUT[$i]=$(printf "%s %10s" "${strOUTPUT[$i]}" "$packets")
+	packets=$(/sbin/ifconfig ${IFACES[$i]} | awk '/RX packets/ {print $3}')
+	rxpkts_0[$i]+=${packets}
+	if [ ! "${calcdeltapkts}" = true ]
+	then
+		strOUTPUT[$i]=$(printf "%s %9s" "${strOUTPUT[$i]}" "$packets")
+	fi 
 done
+
+
+#If calculating delta packets, run through packet count again and do the math after the delay
+if [ "${calcdeltapkts}" = true ]
+then
+	echo -n "Calculating delta packets..."
+	while [ ${deltaperiod} -gt 0 ]
+	do
+		echo -n "${deltaperiod}..."
+		sleep 1
+		: $((deltaperiod--))
+	done
+	echo
+	for i in "${!IFACES[@]}"
+	do
+		packets=$(/sbin/ifconfig ${IFACES[$i]} | awk '/RX packets/ {print $3}')
+		packetsdelta=$(($packets-${rxpkts_0[$i]}))
+		if [ "${debug}" = true ]; then echo "[DEBUG] packets: ${packets}, pkts_0: ${rxpkts_0[$i]}, deltaP: ${packetsdelta}"; fi
+		strOUTPUT[$i]=$(printf "%s %9s %6s" "${strOUTPUT[$i]}" "$packets" "$packetsdelta")
+	done
+fi
+
+
 
 #Execute airmon-ng once (slow, and requires root)
 #Save this to an array and then just print array and use std tools to 
@@ -141,10 +213,16 @@ done
 
 #Print items - total table
 echo
-#echo -e "Ndx\t Iface\t Phy\t Driver\t\t Mode\t\t Up?\t Channel\t Width\t Packets\t Adapter"
-header=(Ndx Iface Phy Driver Mode Up? Channel Width Center Packets Adapter)
-printf "%3s %10s %5s %12s %9s %5s %13s %5s %8s %10s %s\n" "${header[@]}"
+#echo -e "Ndx\t Iface\t Phy\t Driver\t\t Mode\t\t  Up\t Channel\t Width\t Packets\t Adapter"
 
+if [ "${calcdeltapkts}" = true ]
+then
+	header=(Ndx Iface Phy Driver Mode Up Channel Width Center Packets DeltaP Adapter)
+	printf "%3s %9s %5s %12s %10s %3s %13s %6s %8s %9s %6s %s\n" "${header[@]}"
+else
+	header=(Ndx Iface Phy Driver Mode Up Channel Width Center Packets Adapter)
+	printf "%3s %9s %5s %12s %10s %3s %13s %6s %8s %9s %s\n" "${header[@]}"
+fi
 for i in "${strOUTPUT[@]}"
 do
    echo -e "$i" 
@@ -194,7 +272,39 @@ Ndx      Iface   Phy       Driver      Mode   Up?         Channel Width      Pac
 Ndx      Iface   Phy       Driver      Mode   Up?         Channel Width      Packets
   0     wlp1s0  phy0    rt2800pci   managed     Y                                  0
 
+ 
+admin1@dinm9:~/software/injection$ sudo ./interfaces.sh -d 3
+[CONFIG] deltaperiod set to argument 3
+Calculating delta packets...3...2...1...
+[DEBUG] packets: 12185972, pkts_0: 12185881, deltaP: 91
+[DEBUG] packets: 0, pkts_0: 0, deltaP: 0
+[DEBUG] packets: 12694106, pkts_0: 12693937, deltaP: 169
+[DEBUG] packets: 9878849, pkts_0: 9878730, deltaP: 119
+[DEBUG] packets: 10056459, pkts_0: 10056316, deltaP: 143
+[DEBUG] packets: 12174753, pkts_0: 12174593, deltaP: 160
+[DEBUG] packets: 12619865, pkts_0: 12619701, deltaP: 164
+[DEBUG] packets: 12266408, pkts_0: 12266243, deltaP: 165
+[DEBUG] packets: 12106160, pkts_0: 12105997, deltaP: 163
+[DEBUG] packets: 10030207, pkts_0: 10030095, deltaP: 112
+[DEBUG] packets: 12320844, pkts_0: 12320684, deltaP: 160
+[DEBUG] packets: 3993749, pkts_0: 3993628, deltaP: 121
+
+Ndx     Iface   Phy       Driver       Mode  Up       Channel  Width   Center   Packets DeltaP Adapter
+  0       mon0  phy0      iwlwifi   monitor   Y 100 (5500MHz) 160MHz 5570 MHz  12185972     91  00.0 Network controller: Intel Corporation Device 2725 (rev 1a)
+  1      wlan0  phy0      iwlwifi   managed   N                                       0      0  00.0 Network controller: Intel Corporation Device 2725 (rev 1a)
+  2      wlan1  phy1    rtl88XXau   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12694106    169  Senao EUB1200AC AC1200 DB [Realtek RTL8812AU]
+  3      wlan2  phy2     carl9170   monitor   Y   6 (2437MHz)  20MHz 2437 MHz   9878849    119  CACE Technologies Inc. AirPcap NX [Atheros AR9170+AR9104]
+  4      wlan3  phy3    rtl88XXau   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  10056459    143  Realtek Semiconductor Corp. RTL8814AU 802.11a/b/g/n/ac
+  5      wlan4  phy4      mt76x2u   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12174753    160  MediaTek Inc. MT7612U 802.11a/b/g/n/ac
+  6      wlan5  phy5      mt7921u   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12619865    164  MediaTek Inc. Wireless_Device
+  7      wlan6  phy6    ath9k_htc   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12266408    165  Qualcomm Atheros Communications AR9271 802.11n
+  8      wlan7  phy7    rt2800usb   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12106160    163  Ralink Technology, Corp. RT5370
+  9      wlan8  phy8    rt2800usb   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  10030207    112  Ralink Technology, Corp. RT5372
+ 10      wlan9  phy9    rt2800usb   monitor   Y   6 (2437MHz)  20MHz 2437 MHz  12320844    160  NetGear, Inc. WNDA4100 802.11abgn 3x3:3 [Ralink RT3573]
+ 11     wlan10 phy10     carl9170   monitor   Y   6 (2437MHz)  20MHz 2437 MHz   3993749    121  Qualcomm Atheros Communications AR9170 802.11n
+ 
   
 END
+
 
 
